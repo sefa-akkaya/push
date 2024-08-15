4c9218401b0a8ce0b301b6c4e94d251bcfceb259
sefa-akkaya <94069712+sefa-akkaya@users.noreply.github.com>
2024-08-15 10:11:57 +0300

    TASK-11 denemetask123

--- main.py
+++ main.py
@@ -9,4 +9,22 @@ def fun(a,b)
 a+b
 return c
 
+printf("sss")
+def fun(a,b)
+a+b
+return c
+
+def fun(a,b)
+a+b
+return c
+
+printf("sss")
+def fun(a,b)
+a+b
+return c
+
+def fun(a,b)
+a+b
+return c
+
 printf("sss)
