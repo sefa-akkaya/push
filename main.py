826ef6f27c7ef0be9be797a253375e5341ce6755
sefa-akkaya <94069712+sefa-akkaya@users.noreply.github.com>
2024-08-15 10:16:48 +0300

    TASK-11 denemetask

--- main.py
+++ main.py
@@ -10,8 +10,16 @@ a+b
 return c
 
 printf("sss")
+return c
+return c
+
 def fun(a,b)
 a+b
+return c
+
+while
+printf("sss")
+return c
 return c
 
 def fun(a,b)
@@ -19,12 +27,6 @@ a+b
 return c
 
 printf("sss")
-def fun(a,b)
-a+b
-return c
-
-def fun(a,b)
-a+b
 return c
 
 printf("sss)
