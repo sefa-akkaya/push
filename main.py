598bc8a07ce17efed1cddfb71e2ab2c97a0cfb90
sefa-akkaya <94069712+sefa-akkaya@users.noreply.github.com>
2024-08-15 10:02:40 +0300

    TASK-11 asdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd

--- main.py
+++ main.py
@@ -1,31 +1,7 @@
 printf("adas")
-def fun(a,b)
-a+b
-return c
-
-import safsdafasf
-printF()
-def fun(a,b)
-a+b
-return cdef fun(a,b)
-a+b
-return cdef fun(a,b)
-a+b
-return c
+def fun(a,b
 
 def fun(a,b)
 a+b
 return c
 
-def fun(a,b)
-a+b
-return c
-def fun(a,b)
-a+b
-return cdef fun(a,b)
-a+b
-return cdef fun(a,b)
-a+b
-return cdef fun(a,b)
-a+b
-return c
