9e151bd8d442e233a9e6048405fc0ba7d5311ea8
sefa-akkaya <94069712+sefa-akkaya@users.noreply.github.com>
2024-08-15 10:20:47 +0300

    TASK-11 denemetask123

--- main.py
+++ main.py
@@ -1,32 +1,7 @@
 printf("adas")
-def fun(a,b):
 
-def fun(a,b):
-  a+b
-return c
-
-def fun(a,b)
-a+b
-return c
+def fun(a, b):
+    c = a + b
+    return c
 
 printf("sss")
-return c
-return c
-
-def fun(a,b)
-a+b
-return c
-
-while
-printf("sss")
-return c
-return c
-
-def fun(a,b)
-a+b
-return c
-
-printf("sss")
-return c
-
-printf("sss)
